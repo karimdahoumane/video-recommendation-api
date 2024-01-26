3154c0b8ad4ae716ed8ee81fbde9d1464f43ac4a
Karim Dahoumane <krmdahoumane@gmail.com>
2024-01-26 08:42:15 +0100

    🚀Init project

---
+++ src/main/java/com/api/videorecommendation/VideoRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.api.videorecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/videorecommendation/VideoRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.videorecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
